6a5cd088f65d4845d9dd34b4ad97ee330202a749
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-23 08:42:03 -0600

    Added another route which can look for a specific id, and added its template file ejs.

--- express_server.js
+++ express_server.js
@@ -19,7 +19,13 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
     const templateVars = {urls: urlDatabase}
     res.render('urls_index', templateVars);
-})
+});
+app.get("/urls/:id", (req, res) => {
+
+    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+    res.render("urls_show", templateVars);
+
+});
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello<b>World</b></body></html>\n")
