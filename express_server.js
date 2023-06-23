ee8ea071458b501dc23ee8c93a4231e7d4a80f44
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-23 14:37:01 -0600

    I redirected my browser to page after a user enter a longurl

--- express_server.js
+++ express_server.js
@@ -10,7 +10,12 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
 };
 function generateRandomString() {
-    
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultNumber = '';
+    for (let i = 0; i < 6; i++) {
+        resultNumber += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return resultNumber;
 }
 //first Routes
 app.get("/", (req, res) => {
@@ -20,23 +25,31 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+//show our data
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render('urls_index', templateVars);
 });
+// to enter new though form
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
+//show a data to a specific id.
 app.get("/urls/:id", (req, res) => {
 
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
 
 });
+
 app.post("/urls", (req, res) => {
-    console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
-  });
+    console.log(req.body); // Log the POST request body to the console for us to see
+    const shortUrl = generateRandomString();// get unique id by calling function.
+    urlDatabase[id] = req.body.longURL; // save longURL from submissions and generate id, store in urlDatabase
+    console.log(urlDatabase); //for us to see.
+    res.redirect(`/urls/${id}`); // Redirects to new page for longURL and shortURL
+});
+
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello<b>World</b></body></html>\n");
