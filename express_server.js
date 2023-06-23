dc33a767ffadd464a44aaad05fb247d51dbddb66
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-23 12:49:36 -0600

    Added partials again as last one was giving me error

--- express_server.js
+++ express_server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+    "9sm5xK": "http://www.google.com"
 };
 //first Routes
 app.get("/", (req, res) => {
@@ -17,18 +17,18 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 app.get("/urls", (req, res) => {
-    const templateVars = {urls: urlDatabase}
+    const templateVars = { urls: urlDatabase };
     res.render('urls_index', templateVars);
 });
 app.get("/urls/:id", (req, res) => {
 
-    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
 
 });
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello<b>World</b></body></html>\n")
+    res.send("<html><body>Hello<b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
