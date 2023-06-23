92b7b6c1694eb1e4a75d520f798a5a7bba408046
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-23 08:14:25 -0600

    I added ejs file index-urls.ejs and added some html, bootstrap codes into it.

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+    const templateVars = {urls: urlDatabase}
+    res.render('urls_index', templateVars);
+})
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello<b>World</b></body></html>\n")
