d818f2243d14087713d95f872aa32884bcff29a3
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-25 10:01:21 -0600

    I fixed my mistakes creating from show.ejs file about my input.

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+//To create unique Id
 function generateRandomString() {
     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let resultNumber = '';
@@ -18,6 +19,7 @@ function generateRandomString() {
     return resultNumber;
 }
 //first Routes
+//first very home page
 app.get("/", (req, res) => {
     res.send("Welcome to my Tinyapp");
 });
@@ -30,7 +32,7 @@ app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render('urls_index', templateVars);
 });
-// to enter new though form
+// to enter new info though form
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
@@ -45,11 +47,35 @@ app.get("/urls/:id", (req, res) => {
 app.post("/urls", (req, res) => {
     console.log(req.body); // Log the POST request body to the console for us to see
     const shortUrl = generateRandomString();// get unique id by calling function.
-    urlDatabase[id] = req.body.longURL; // save longURL from submissions and generate id, store in urlDatabase
+    urlDatabase[shortUrl] = req.body.longURL; // save longURL from submissions and generate id, store in urlDatabase
     console.log(urlDatabase); //for us to see.
-    res.redirect(`/urls/${id}`); // Redirects to new page for longURL and shortURL
+    res.redirect(`/urls/${shortUrl}`); // Redirects to new page for longURL and shortURL
 });
+// Route for /urls/:id to handle editing of Short URL ID details
+app.post(`/urls/:id`, (req, res) => {
+    // Get the id from the route parameter
+    const id = req.params.id;
+    // Assign longURL value to be the editURL value received from the POST request body
+    const newLongURL = req.body.longURL;
+    // Update the longUrl value in the database with the new value
+    urlDatabase[id] = newLongURL;
+    // Redirect user to /urls
+    res.redirect(`/urls`);
+});
+app.post("/urls/:id/delete", (req, res) => {
+    const ids = req.params.id;
+    for (let keys in urlDatabase) {
+        if (keys === ids) {
+            delete urlDatabase[keys];
+        }
+    }
+    //     const requestID = req.params.id;
 
+    //     if (urlDatabase[requestID]) {
+    //     delete urlDatabase[requestID]
+    // }
+    res.redirect(`/urls`);
+});
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello<b>World</b></body></html>\n");
