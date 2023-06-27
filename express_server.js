c497fdfb9f0f36c88c672e4f48a01ece3c9b33d1
Rabia Abdur Rehman <rabia.abdur.rehman@gmail.com>
2023-06-26 22:58:13 -0600

    Modified with registration form

--- express_server.js
+++ express_server.js
@@ -5,11 +5,23 @@ const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
 app.set("view engine", "ejs");
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));// for submitting forms.
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
+};
+const users = {
+    userRandomID: {
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur",
+    },
+    user2RandomID: {
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk",
+    },
 };
 //To create unique Id
 function generateRandomString() {
@@ -31,20 +43,26 @@ app.get("/urls.json", (req, res) => {
 });
 //show our data
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+    const user = users[req.cookies['user_id']];
+    const templateVars = { urls: urlDatabase, user: user };
     res.render('urls_index', templateVars);
 });
 // to enter new info though form
 app.get("/urls/new", (req, res) => {
-    const templateVars = {username: req.cookies["username"] }
+    const user = users[req.cookies['user_id']];
+    const templateVars = { user: user };
     res.render("urls_new", templateVars);
 });
 //show a data to a specific id.
 app.get("/urls/:id", (req, res) => {
-
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+    const user = users[req.cookies['user_id']];
+    console.log(user);
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: user };
     res.render("urls_show", templateVars);
 
+});
+app.get("/register", (req, res) => {
+    res.render('registration.ejs');
 });
 
 app.post("/urls", (req, res) => {
@@ -72,11 +90,6 @@ app.post("/urls/:id/delete", (req, res) => {
             delete urlDatabase[keys];
         }
     }
-    //     const requestID = req.params.id;
-
-    //     if (urlDatabase[requestID]) {
-    //     delete urlDatabase[requestID]
-    // }
     res.redirect(`/urls`);
 });
 app.post("/Login", (req, res) => {
@@ -86,9 +99,21 @@ app.post("/Login", (req, res) => {
 });
 app.post("/Logout", (req, res) => {
     console.log('Logout button clicked');
-    res.clearCookie('username');
+    res.clearCookie('user_id');
     res.redirect('/urls');
-  });
+});
+app.post("/register", (req, res) => {
+    const newUser = {};
+    const userId = generateRandomString();
+    users[userId] = {
+        id: userId,
+        email: req.body.email,
+        password: req.body.password
+    };
+    res.cookie("user_id", userId);
+    //console.log("is it clicked");
+    res.redirect("/urls");
+});
 //To check how we put html code in res.
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello<b>World</b></body></html>\n");
